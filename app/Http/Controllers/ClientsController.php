87310c4a50ae3e6ca33c4bc8a874e194b9e5d1c8
Adriano API <adrianoapi@hotmail.com>
2018-08-06 23:11:48 -0300

    add controller actions Client

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -2,11 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Client;
 use Illuminate\Http\Request;
 
 class ClientsController extends Controller
 {
 
+	public function listar()
+	{
+		$clients = Client::all();
+        return view('admin.cliente.list', compact('clients'));
+	}
+
+	public function formCadastrar()
+	{
+		return view('admin.cliente.create');
+	}
+
+	public function cadastrar(Request $request)
+	{
+		$client = new Client();
+        $client->name  = $request->name;
+        $client->email = $request->email;
+        $client->save();
+        return redirect()->to('/admin/client');
+	}
+
+	public function formEditar($id)
+	{
+		$client = Client::find($id);
+
+		if(!$client){
+            abort(404);
+        }
+
+		return view('admin.cliente.edit', compact('client'));
+	}
+
+	public function editar(Request $request, $id)
+    {
+		$client = Client::find($id);
+
+        if(!$client){
+            abort(404);
+        }
+
+        $client->name = $request->name; //$request->get('name')
+        $client->email = $request->email;
+        $client->save();
+        return redirect()->to('/admin/client');
+    }
+
+    public function excluir(Request $request, $id)
+    {
+    	$client = Client::find($id);
+    	
+        if(!$client){
+            abort(404);
+        }
+
+        $client->delete();
+        return redirect()->to('/admin/client');
+    }
+
+
+/*
     public function cadastrar()
     {
     	//csrf-token - gerado no servidor e inserido no form do usuário
@@ -15,15 +75,7 @@ class ClientsController extends Controller
 				->with('csrftoken', $csrftoken)
 				->with('name', 'Adriano');
     }
-
-    public function exluir()
-    {
-
-    }
-
-    public function editar()
-    {
-
-    }
+*/
+    
 
 }
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,12 @@ Route::group(['prefix' => '/'], function(){
 
 // Admin
 Route::group(['prefix'=> '/admin'], function(){
-	Route::get('/cliente/cadastrar', 'ClientsController@cadastrar');
+	Route::get('client', 'ClientsController@listar');
+	Route::get('client/form-cadastrar', 'ClientsController@formCadastrar');
+    Route::post('client/cadastrar', 'ClientsController@cadastrar');
+    Route::get('client/{id}/form-editar', 'ClientsController@formEditar');
+    Route::post('client/{id}/editar', 'ClientsController@editar');
+    Route::get('client/{id}/excluir', 'ClientsController@excluir');
 });
 
 
